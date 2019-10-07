1bd23b943c08e301cde4f563ab361181050286a2
Ronald Pandolfi <ronpandolfi@gmail.com>
2019-10-07 15:23:41 -0700

    Add category getter to processingplugin

--- xicam/plugins/processingplugin.py
+++ xicam/plugins/processingplugin.py
@@ -250,6 +250,10 @@ class ProcessingPlugin(IPlugin):
             self._param.sigValueChanged.connect(self.setParameterValue)
         return self._param
 
+    @staticmethod
+    def getCategory() -> str:
+        return "default"
+
     def setParameterValue(self, name, param, value):
         """
         Sets the `name` parameter's value to the passed value.
